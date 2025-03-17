dc9035581d8bf2f9feb69b43bc744661ed2a7112
Artem <artemohrimenko23@gmail.com>
2025-03-17 22:05:21 +0200

    add validate.min.js and maskedinput.min.js file, added a mailer to the project and configured mail sending

--- src/js/script.js
+++ src/js/script.js
@@ -33,6 +33,16 @@ $(document).ready(function () {
         }
     );
 
+    document.addEventListener("DOMContentLoaded", function () {
+        // Получаем все кнопки с ролью "tab"
+        let buttons = document.querySelectorAll('button[role="tab"]');
+
+        // Проходим по всем кнопкам и удаляем их содержимое
+        buttons.forEach(function (button) {
+            button.textContent = ""; // Удаляем текст из кнопки
+        });
+    });
+
     function toggleSlide(item) {
         $(item).each(function (i) {
             $(this).on("click", function (e) {
@@ -49,14 +59,100 @@ $(document).ready(function () {
 
     toggleSlide(".catalog-item__link");
     toggleSlide(".catalog-item__link-back");
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Получаем все кнопки с ролью "tab"
-    let buttons = document.querySelectorAll('button[role="tab"]');
+    // modal
 
-    // Проходим по всем кнопкам и удаляем их содержимое
-    buttons.forEach(function (button) {
-        button.textContent = ""; // Удаляем текст из кнопки
+    $("[data-modal=consultation]").on("click", function () {
+        $(".overlay, #consultation").fadeIn("slow");
+    });
+    $(".modal__close").on("click", function (e) {
+        $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+    });
+
+    $(".button__mini").each(function (i) {
+        $(this).on("click", function () {
+            $("#order .modal__descr").text(
+                $(".catalog-item__subtitle").eq(i).text()
+            );
+            $(".overlay, #order").fadeIn("slow");
+        });
+    });
+
+    // closing the modal window when clicking anywhere on the screen except for the button
+
+    $(window).on("click", function (e) {
+        if (e.target.classList.contains("overlay")) {
+            $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+        }
+    });
+
+    // closing the modal window when pressing Escape
+
+    $(document).keydown(function (e) {
+        if (e.key === "Escape") {
+            $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+        }
+    });
+
+    // form validation
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2,
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!"),
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты",
+                },
+            },
+        });
+    }
+
+    validateForms("#consultation-form");
+    validateForms("#consultation form");
+    validateForms("#order form");
+
+    // input mask
+
+    $("input[name=phone]").mask("+38 (999) 999-99-99");
+
+    // sending letters from the site
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        })
+            .done(function () {
+                $(this).find("input").val("");
+
+                $("#consultation, #order").fadeOut();
+                $(".overlay, #thanks").fadeIn("slow");
+
+                $("form").trigger("reset");
+            });
+        return false;
     });
 });
---
+++ src/mailer/smart.php
@@ -0,0 +1,48 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.ukr.net';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'mj23fakeemail@ukr.net';                 // Наш логин
+$mail->Password = 'bWvtl9mEiUFKHOBi';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('mj23fakeemail@ukr.net', 'Pulse');   // От кого письмо 
+$mail->addAddress('mj23fakeemail@ukr.net');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+	if(!$mail->send()) {
+		echo 'Ошибка отправки: ' . $mail->ErrorInfo;
+		return false;
+	} else {
+		echo 'Письмо успешно отправлено';
+		return true;
+	}
+
+?>
