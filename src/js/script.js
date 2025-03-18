e22dca1bc317ace6745e1b90b2d65de480084705
Artem <artemohrimenko23@gmail.com>
2025-03-18 16:21:45 +0200

    added the Up button and smooth scrolling through the links

--- src/js/script.js
+++ src/js/script.js
@@ -143,16 +143,34 @@ $(document).ready(function () {
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
-            data: $(this).serialize()
-        })
-            .done(function () {
-                $(this).find("input").val("");
+            data: $(this).serialize(),
+        }).done(function () {
+            $(this).find("input").val("");
 
-                $("#consultation, #order").fadeOut();
-                $(".overlay, #thanks").fadeIn("slow");
+            $("#consultation, #order").fadeOut();
+            $(".overlay, #thanks").fadeIn("slow");
 
-                $("form").trigger("reset");
-            });
+            $("form").trigger("reset");
+        });
+        return false;
+    });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1200) {
+            $(".pageup").fadeIn();
+        } else {
+            $(".pageup").fadeOut();
+        }
+    });
+
+    // smooth scroll through links
+
+    $("a[href^='#']").on("click", function (event) {
+        event.preventDefault();
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
         return false;
     });
 });
